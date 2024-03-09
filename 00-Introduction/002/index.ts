cac28663bbfb3cc39c389016706c0995702a58aa
mrfiratatalay <atafiratbey@gmail.com>
2024-03-09 21:12:10 +0300

    TypeScript Journey

---
+++ 00-Introduction/002/index.ts
@@ -0,0 +1,11 @@
+let movieTitle: string = "Benim Adim Gultepe";
+movieTitle = "Bir Ask Hikayesi";
+// movieTitle = 9;   When I type this way then am gonna get error!!
+movieTitle.toUpperCase; //  I can call this method on my string but
+
+/*
+
+movieTitle = 13;
+movieTitle.toUpperCase; // now it is not working because the variable has been changed to number!
+
+*/
