af66eab01a35fbeb59c03173190c81859fc86a20
mrfiratatalay <atafiratbey@gmail.com>
2024-03-09 21:21:31 +0300

    TypeScript Journey

---
+++ 00-Introduction/003/index.js
@@ -0,0 +1,8 @@
+//! NUMBERS:
+var numCatLives = 13;
+numCatLives += 13;
+// numCatLives = "firat";
+//! BOOLEANS:
+var gameOver = false;
+gameOver = true;
+// gameOver = "true";
---
+++ 00-Introduction/003/index.ts
@@ -0,0 +1,11 @@
+//! NUMBERS:
+let numCatLives: number = 13;
+numCatLives += 13;
+
+// numCatLives = "firat";
+
+//! BOOLEANS:
+let gameOver: boolean = false;
+gameOver = true;
+
+// gameOver = "true";
