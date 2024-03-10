fbc9cddf9deb5ecea967967f5c7253cebb703146
mrfiratatalay <atafiratbey@gmail.com>
2024-03-10 23:08:54 +0300

    TypeScript Journey

--- 00-Introduction/005/index.ts
+++ 00-Introduction/005/index.ts
@@ -4,3 +4,14 @@ thing = 1;
 thing = false;
 thing();
 thing.toUpperCase();
+
+// any type is turn off type checking:
+
+const movies = ["one", "two", "three", "four"];
+let foundMovie;
+
+for (let movie of movies) {
+  if (movie === "three") {
+    foundMovie = "three";
+  }
+}
