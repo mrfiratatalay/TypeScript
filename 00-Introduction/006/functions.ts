2ca2729f3a820e5d87ffa697065d4e5684f06e6c
mrfiratatalay <atafiratbey@gmail.com>
2024-03-10 23:48:33 +0300

    TypeScript Journey

--- 00-Introduction/006/functions.ts
+++ 00-Introduction/006/functions.ts
@@ -1,23 +1,44 @@
 // Part-1
-function square(num: number) {
+// function square(num: number) {
+//   return num * num;
+// }
+
+// function greet(person: string) {
+//   return `Hi there, ${person}!`;
+// }
+
+// square(13);
+// greet("Firat");
+
+// Part-2
+// const doSomething = (person: string, age: number, isFunny: boolean) => {
+//   return `Hi there, ${person}. You are ${age}. That's ${isFunny}`;
+// };
+
+// doSomething("Firat", 13, true);
+
+// Part-3
+// function message(person: string = "FIRAT") {
+//   return `Hi there, ${person}`;
+// }
+
+// Part-4
+function greet(person: string): string {
+  return `Hi there, ${person}!`;
+}
+function square(num: number): number {
   return num * num;
 }
 
-function greet(person: string) {
-  return `Hi there, ${person}!`;
-}
-
-square(13);
-greet("Firat");
-
-// Part-2
-const doSomething = (person: string, age: number, isFunny: boolean) => {
-  return `Hi there, ${person}. You are ${age}. That's ${isFunny}`;
+const add = (x: number, y: number): number => {
+  return x + y;
 };
 
-doSomething("Firat", 13, true);
-
-// Part-3
-function message(person: string = "FIRAT") {
-  return `Hi there, ${person}`;
+function random(num: number) {
+  if (Math.random() < 0.5) {
+    return num.toString();
+  }
+  return num;
 }
+
+// Part-5
